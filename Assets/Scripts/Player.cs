f244141a3473fe0ecbe4f6bdedd3e64eb9e84ef5
rhakt <xreinf.fnk@gmail.com>
2016-09-05 19:44:18 +0900

    up down

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    Rigidbody2D rig;
+    Vector2 size;
+
+    // Use this for initialization
+    void Start () {
+        rig = GetComponent<Rigidbody2D>();
+        var sr = GetComponent<SpriteRenderer>();
+        size = sr.bounds.size;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Move();
+	}
+
+    void Move()
+    {
+        var vy = Input.GetAxis("Vertical");
+        rig.AddForce(new Vector2(0, vy), ForceMode2D.Force);
+
+        Clamp();
+
+    }
+
+    void Clamp()
+    {
+        // 画面左下,右上のワールド座標をビューポートから取得
+        Vector2 min = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
+        Vector2 max = Camera.main.ViewportToWorldPoint(new Vector2(0, 1));
+
+        Vector2 pos = transform.position;
+        min.y += size.y / 2;
+        max.y -= size.y / 2;
+
+        var top = pos.y < min.y;
+        var bottom = pos.y > max.y;
+        
+        // Mathf.Clamp(pos.y, min.y, max.y);
+        if (top || bottom)
+        {
+            rig.velocity = new Vector2(0, 0);
+            pos.y = top ? min.y : max.y;
+            transform.position = pos;
+        }
+    }
+
+}
